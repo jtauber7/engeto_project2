a1a743d8d880b0e52ffd45edef30cb44c802207d
Honza <jtauber@seznam.cz>
2019-11-18 01:38:49 +0100

    change according to comments, Bulls - input valuation for 4 different digits TicTac - input corrections, input check function

--- Bulls_Cows.py
+++ Bulls_Cows.py
@@ -5,21 +5,23 @@ import time
 def generate_digits():
     digits = []
     while len(digits) < 4:
-        random_digit = random.randint(0,9)
+        random_digit = random.randint(0, 9)
         if random_digit not in digits:
             digits.append(random_digit)
     return digits
 
 
 def ask_number():
-    print("Enter a number")
-    num = input()
-    return (list(int(i) for i in num))
+    while True:
+        number = input("Enter a number\n")
+        if number.isdigit() and len(set(number)) == 4:
+            return (list(int(i) for i in number))
+        print("You must enter an 4 four digits number with different digits.")
 
 
-def evaulate_num(guess_num,random_digits):
+def evaulate_number(guess_number, random_digits):
     result = [0, 0]
-    for index, num in enumerate(guess_num):
+    for index, num in enumerate(guess_number):
         if num not in random_digits:
             continue
         if num == random_digits[index]:
@@ -33,18 +35,18 @@ def result_print(result):
     print("{} bulls, {} cows".format(*result))
 
 
-def end_game(attemp,time_to_guess):
-    if attemp < 5:
+def end_game(attempt,time_to_guess):
+    if attempt < 5:
         evaulation = "amazing"
-    elif attemp < 10:
+    elif attempt < 10:
         evaulation = "average"
-    elif attemp < 14:
+    elif attempt < 14:
         evaulation = "not so good"
     else:
         evaulation = "really bad"
-    print(f"Correct, you've guessed the right number in {attemp} guesses")
-    print(f"Your time to guess the number is {time_to_guess} seconds.")
-    print(f"That's {evaulation} !!!")
+    print(f"Correct, you've guessed the right number in {attempt} guesses")
+    print(f"Your time to guess the number was {time_to_guess} seconds.")
+    print(f"That's {evaulation}!!!")
 
 
 def main():
@@ -52,19 +54,19 @@ def main():
     print("""Hi there!
 I've generated a random 4 digit number for you.
 Let's play a bulls and cows game.""")
-    guess_num = 0
-    attemp = 0
+    guess_number = 0
+    attempt = 0
     start_time = time.time()
-    while guess_num != random_digits:
-        attemp += 1
-        guess_num = ask_number()
-        result = evaulate_num(guess_num,random_digits)
+    while guess_number != random_digits:
+        attempt += 1
+        guess_number = ask_number()
+        result = evaulate_number(guess_number, random_digits)
         result_print(result)
         print("=" * 15)
         print()
     end_time = time.time()
     time_to_guess = int(end_time - start_time)
-    end_game(attemp, time_to_guess)
+    end_game(attempt, time_to_guess)
 
 
 main()
